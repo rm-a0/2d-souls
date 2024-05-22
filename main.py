85429f0f2e8fec2beaa9e02056e0427afd7c3cfd
rm-a0 <michalrepcik9@gmail.com>
2024-05-22 11:06:47 +0200

    added movement, jumping, defined new constants

--- main.py
+++ main.py
@@ -18,6 +18,17 @@ def main():
             # QUIT event
             if event.type == pygame.QUIT: 
                 running = False
+        
+        # Controls
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_d]:
+            p.move_right()
+        if keys[pygame.K_a]:
+            p.move_left()
+        if keys[pygame.K_SPACE]:
+            p.jump()
+        
+
 
         # Clear and draw new frame
         screen.fill('black')
--- objs/player.py
+++ objs/player.py
@@ -5,7 +5,7 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
         self.image = pygame.Surface((50, 70))
-        self.image.fill('white')
+        self.image.fill('green')
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
         # Player stats
@@ -13,3 +13,18 @@ class Player(pygame.sprite.Sprite):
         self.mana = PLAYER_MANA
         self.stamina = PLAYER_STAMINA
         self.speed = PLAYER_SPEED
+        # Flags
+        self.jumping = False
+        self.rolling = False
+        self.facing = RIGHT
+
+    def move_right(self):
+        self.rect.x += self.speed
+        self.facing = RIGHT
+
+    def move_left(self):
+        self.rect.x -= self.speed
+        self.facing = LEFT
+
+    def jump(self):
+        self.rect.y -= self.speed
