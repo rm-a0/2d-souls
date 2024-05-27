b0e6b45c84f67ee0cf04a6d668f20f92aa0ad2aa
rm-a0 <michalrepcik9@gmail.com>
2024-05-27 13:11:53 +0200

    removed redudndant class atr and adjusted constants

--- objs/enemy.py
+++ objs/enemy.py
@@ -13,11 +13,7 @@ class Enemy(pygame.sprite.Sprite):
         self.vel_y = 0
         # Enemy stats
         self.max_hp = ENEMY_MAX_HP 
-        self.max_mana = ENEMY_MAX_MANA
-        self.max_stamina = ENEMY_MAX_STAMINA
         self.hp = ENEMY_MAX_HP
-        self.mana = ENEMY_MAX_MANA
-        self.stamina = ENEMY_MAX_STAMINA
         self.speed = ENEMY_SPEED
 
     # Decreases hp by certain amount
